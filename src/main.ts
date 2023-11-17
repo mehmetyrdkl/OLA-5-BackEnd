1a0f74dfaf1bad4b82913f37336f1f46ed852e2c
ThordurTh <thordur921@gmail.com>
2023-11-17 16:12:32 +0100

    cors problem

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(express()), // Using Express instance
+  );
+
+  // Enable CORS for all origins
+  app.enableCors();
+
   await app.listen(8080);
 }
 bootstrap();
