a0d193c71a02a308086d56b6dfff6e787c423064
ThordurTh <thordur921@gmail.com>
2023-12-04 11:04:05 +0100

    get booking endpoint
    Co-authored-by: Mehmet Yurdakul <mehmetyrdkl@users.noreply.github.com>
    Co-authored-by: Martin Windsor <windsorm@users.noreply.github.com>

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,15 +1,21 @@
 // bookings.service.ts
-import { Injectable, BadRequestException } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Booking, BookingDocument } from './schemas/booking.schema';
 import { CreateBookingDto } from './dto/create-booking.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class BookingService {
   constructor(
     @InjectModel(Booking.name)
     private readonly bookingModel: Model<BookingDocument>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createBooking(createBookingDto: CreateBookingDto): Promise<Booking> {
@@ -33,6 +39,37 @@ export class BookingService {
         });
       }
 
+      throw new BadRequestException({
+        status: 500,
+        message: 'Internal Server Error',
+      });
+    }
+  }
+
+  async getBookingByUserId(token: string): Promise<Booking[] | null> {
+    try {
+      const decodedToken = this.jwtService.verify(token);
+
+      if (!decodedToken.id) {
+        throw new BadRequestException({
+          status: 400,
+          message: 'Invalid user information in the token',
+        });
+      }
+
+      const bookings = await this.bookingModel
+        .find({ user_id: decodedToken.id })
+        .exec();
+
+      if (!bookings || bookings.length === 0) {
+        throw new NotFoundException({
+          status: 404,
+          message: 'Bookings not found',
+        });
+      }
+
+      return bookings;
+    } catch (error) {
       throw new BadRequestException({
         status: 500,
         message: 'Internal Server Error',
